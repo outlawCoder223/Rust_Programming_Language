cae9723163322b1611af89a5426b58b2ecd29707
Rance Campbell <rance@outlawcoding.com>
2021-09-30 06:25:49 -0600

    finish ch10

---
+++ ch10/generics/src/main.rs
@@ -0,0 +1,63 @@
+use std::cmp::PartialOrd;
+use std::fmt::Display;
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn longest_with_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("the largest number is {}", result);
+
+    let char_list = vec!['r', 'a', 'n', 'c', 'e'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+}
